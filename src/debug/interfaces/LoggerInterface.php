cd60a1c953142944140b506e3ea6c5b29c36b7c3
sts-sys <sts.framework@gmail.com>
2024-09-12 01:09:42 +0200

    Update LoggerInterface.php

--- src/debug/interfaces/LoggerInterface.php
+++ src/debug/interfaces/LoggerInterface.php
@@ -4,6 +4,7 @@ namespace sts\debug\interfaces;
 
 interface LoggerInterface
 {
+    // Metodele de bază pentru logare
     public function log($level, $message, array $context = []);
     public function debug($message, array $context = []);
     public function info($message, array $context = []);
@@ -11,4 +12,11 @@ interface LoggerInterface
     public function warning($message, array $context = []);
     public function error($message, array $context = []);
     public function critical($message, array $context = []);
+    
+    // Metode pentru funcționalități suplimentare
+    public function setFormatter(callable $formatter);
+    public function registerErrorHandlers();
+    public function addCallback(callable $callback);
+    public function addFilter(callable $filter);
+    public function setRetentionPolicy(int $days);
 }
